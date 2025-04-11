6560385723403f98dc44159b908c68e76ba7c86c
Rahul <chrahulofficial@gmail.com>
2025-04-11 19:44:46 +0530

    done

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,25 +1,48 @@
-import React from "react";
-import { FaHome, FaShoppingCart, FaSignOutAlt } from "react-icons/fa";
+import React, { useState } from "react";
+import {
+  FaHome,
+  FaShoppingCart,
+  FaSignOutAlt,
+  FaBars,
+  FaTimes,
+} from "react-icons/fa";
 import "./Navbar.css";
-
+import { Link } from "react-router-dom";
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
-      <div className="navbar-logo">MINIMAL</div>
-      <ul className="navbar-links">
-        <li>
-          <FaHome className="icon" />
-          <span>Home</span>
-        </li>
-        <li>
-          <FaShoppingCart className="icon" />
-          <span>Cart</span>
-        </li>
-        <li>
-          <FaSignOutAlt className="icon" />
-          <span>Logout</span>
-        </li>
-      </ul>
+      <div className="navbar-container">
+        <div className="navbar-logo">MINIMAL</div>
+        <div className="menu-icon" onClick={toggleMenu}>
+          {isOpen ? <FaTimes className="icon" /> : <FaBars className="icon" />}
+        </div>
+        <ul className={isOpen ? "navbar-links active" : "navbar-links"}>
+          <Link to={"/home"}>
+            <li>
+              <FaHome className="icon" />
+              <span>Home</span>
+            </li>
+          </Link>
+          <Link to={"/cart"}>
+            <li>
+              <FaShoppingCart className="icon" />
+              <span>Cart</span>
+            </li>
+          </Link>
+          <Link to={"/login"}>
+            <li>
+              <FaSignOutAlt className="icon" />
+              <span>Logout</span>
+            </li>
+          </Link>
+        </ul>
+      </div>
     </nav>
   );
 }
